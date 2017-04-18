c90cce3cd91d3c5ff77860e7a68628213300a11e
DereK Han <derekhan206@gmail.com>
2017-04-18 15:30:16 -0700

    Adds completed work code

--- app/src/main/java/edu/uw/loaderdemo/WordListFragment.java
+++ app/src/main/java/edu/uw/loaderdemo/WordListFragment.java
@@ -1,8 +1,16 @@
 package edu.uw.loaderdemo;
 
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.UserDictionary;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +20,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.R.attr.data;
+
 /**
  * A simple Fragment to display a list of words.
  */
-public class WordListFragment extends Fragment {
+public class WordListFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = "WordList";
 
-    private ArrayAdapter<String> adapter;
+    private SimpleCursorAdapter adapter;
 
     public WordListFragment() {
         // Required empty public constructor
@@ -37,16 +47,38 @@ public class WordListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_word_list, container, false);
 
-        //model
-        String[] data = {"Dog","Cat","Android","Inconceivable"};
+        // URI - Uniform Resource Indicator
+        // Log.v(TAG, UserDictionary.Words.CONTENT_URI);
+//        String[] projection = { UserDictionary.Words.WORD, UserDictionary.Words._ID };
+//        Cursor cursor = getActivity().getContentResolver().query(
+//                UserDictionary.Words.CONTENT_URI, projection, null, null, null);
+//
+//        cursor.moveToFirst();
+//        Log.v(TAG, cursor.getString(0));
+//
+//        //model
+//        String[] data = {"Dog","Cat","Sound","Reasonable"};
 
         //controller
         AdapterView listView = (AdapterView)rootView.findViewById(R.id.wordListView);
 
-        adapter = new ArrayAdapter<String>(
-                getActivity(), R.layout.list_item_layout, R.id.txtListItem, data);
+        int[] views = { };
+
+        adapter = new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.list_item_layout,
+                null,
+                new String[] { UserDictionary.Words.WORD },
+                new int[] {R.id.txtListItem},
+                0
+        );
+
+//        adapter = new ArrayAdapter<String>(
+//                getActivity(), R.layout.list_item_layout, R.id.txtListItem, data);
+
         listView.setAdapter(adapter);
 
+        getLoaderManager().initLoader(0, null, this);
 
         //handle button input
         final TextView inputText = (TextView)rootView.findViewById(R.id.txtAddWord);
@@ -56,9 +88,44 @@ public class WordListFragment extends Fragment {
             public void onClick(View v) {
                 String inputWord = inputText.getText().toString();
                 Log.v(TAG, "To add: "+inputWord);
+
+                ContentValues values = new ContentValues(); // a bundle only for databases
+                values.put(UserDictionary.Words.WORD, inputWord);
+                values.put(UserDictionary.Words.APP_ID, "edu.uw.loaderdemo");
+                values.put(UserDictionary.Words.LOCALE, "en_US");
+                values.put(UserDictionary.Words.FREQUENCY, 100);
+
+                // getcontentresolver can interact with the database
+                getActivity().getContentResolver()
+                        .insert(UserDictionary.Words.CONTENT_URI, values);
             }
         });
 
         return rootView;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        // which columns i wnat to be able to get
+        String[] projection = { UserDictionary.Words.WORD, UserDictionary.Words._ID };
+
+        CursorLoader loader = new CursorLoader(
+                getActivity(),
+                UserDictionary.Words.CONTENT_URI,
+                projection, null, null, null
+        );
+
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
     }
 }
